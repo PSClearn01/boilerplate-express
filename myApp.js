c91c8683d885138baf0ebf1e3a76b810c0a3ca07
Puro Sur <purosurbxo@gmail.com>
2024-04-27 02:15:26 -0700

    echo server

--- myApp.js
+++ myApp.js
@@ -1,9 +1,60 @@
-let express = require('express');
-let app = express();
-console.log("Hello World");
+require('dotenv').config()
 
-app.get("/", (req, res) => {
-  res.send("Hello Express");
+var express = require('express');
+var app = express();
+
+
+app.get("/:word/echo", (req, res) => {
+  let word = req.params.word
+  
+  let jsonObj = {echo: word,echo: word};
+  res.send(jsonObj);
+});
+
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({
+      time: req.time
+    });
+  }
+);
+
+app.use(( req, res, next) => {
+
+  let string = `${req.method} ${req.path} - ${req.ip}`
+  console.log(string)
+  
+   next();
+
+});
+
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
+
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+var message= 'Hello json';
+app.get("/json", (req, res) => {
+  if ( process.env['MESSAGE_STYLE'] === "uppercase") {
+      res.json({ "message": message.toUpperCase()  });
+  }
+  else {
+    res.json({ "message": message });
+  }    
 });
 
 
